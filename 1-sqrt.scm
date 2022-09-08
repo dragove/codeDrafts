7e701e7407c45fc181cc73d17c7bbf4c93b62e41
dragove <dragove@qq.com>
2022-09-09 00:24:03 +0800

    optimize code:

--- 1-sqrt.scm
+++ 1-sqrt.scm
@@ -15,22 +15,21 @@
 (assert (= (average 4 6)
            5))
 
-(define (improve guess x)
-                 (average guess (/ x guess)))
-
-(define (good-enough? guess x)
-  (< (abs (- (square guess) x))
-     0.001))
-
-(define (sqrt-iter guess x)
-  (if (good-enough? guess x)
-    guess
-    (sqrt-iter (improve guess x) x)))
-
 (define (sqrt x) 
+    (define (improve guess x)
+            (average guess (/ x guess)))
+
+    (define (good-enough? guess x)
+      (< (abs (- (square guess) x))
+         0.001))
+
+    (define (sqrt-iter guess x)
+      (if (good-enough? guess x)
+        guess
+        (sqrt-iter (improve guess x) x)))
+
   (sqrt-iter 1.0 x))
 
-(trace sqrt-iter)
 
 (display (sqrt 4))
 (newline)
