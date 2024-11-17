da2d9888bfdb43456e6125eaf243ae670b95cf60
dragove <dragove@qq.com>
2024-11-17 21:03:59 +0800

    scala: make wc count stdin

--- scala/wc.scala
+++ scala/wc.scala
@@ -6,8 +6,24 @@
 import mainargs.{main, arg, ParserForMethods, Flag}
 import java.nio.file.*
 import scala.jdk.CollectionConverters.*
+import scala.io.Source
 
 object Main {
+  private def readInput(fileName: Option[String]) = {
+    fileName match
+      case Some(file) =>
+        val path = Paths.get(file)
+        if !Files.exists(path) then
+          System.err.println(s"Error: $fileName not exists")
+          System.exit(1)
+        if Files.isDirectory(path) then
+          System.err.println(s"Error: $fileName is a directory")
+          System.exit(1)
+        Files.readAllLines(path).asScala
+      case None =>
+        Source.stdin.getLines().toBuffer
+  }
+
   private def countWords(s: String) =
     s.split("\\s").count(_.nonEmpty)
 
@@ -17,25 +33,20 @@ object Main {
       @arg(doc = "count lines") l: Flag,
       @arg(doc = "count words") w: Flag,
       @arg(doc = "count characters") m: Flag,
-      @arg(positional = true) fileName: String
+      @arg(positional = true) fileName: Option[String]
   ) = {
-    val path = Paths.get(fileName)
-    if !Files.exists(path) then
-      System.err.println(s"Error: $fileName not exists")
-      System.exit(1)
-    if Files.isDirectory(path) then
-      System.err.println(s"Error: $fileName is a directory")
-      System.exit(1)
+    val lines = readInput(fileName)
     val noFlag = !(c.value || l.value || w.value || m.value)
-    val lines = Files.readAllLines(path).asScala
     val output = Seq(
       if l.value || noFlag then Some(lines.size.toString) else None,
       if w.value || noFlag then Some(lines.map(countWords).sum.toString)
       else None,
-      if c.value || noFlag then Some(Files.size(path).toString) else None,
+      if c.value || noFlag then
+        Some(lines.map(_.getBytes().length + 1).sum.toString)
+      else None,
       if m.value then Some(lines.map(_.length + 1).sum.toString) else None
     ).flatten
-    println((output :+ fileName).mkString(" "))
+    println((fileName.map(output :+ _).getOrElse(output)).mkString(" "))
   }
 
   def main(args: Array[String]): Unit =
